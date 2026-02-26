955afb403e32a89d802ea2d1d2131485a8a3d3af
seonmin <w.hand715@gmail.com>
2026-02-27 02:55:42 +0900

    rename: ProductController -> ProductdAdminController

--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/product/ProductAdminController.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/product/ProductAdminController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-public class ProductController {
+public class ProductAdminController {
 
     private final ProductFacade productFacade;
 
